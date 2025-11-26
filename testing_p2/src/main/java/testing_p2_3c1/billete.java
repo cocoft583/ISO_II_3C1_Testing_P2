9f11845556690810f15429f556af57167edbe64d
coco <jorge.assis@alu.uclm.es>
2025-11-26 12:54:41 +0100

    Implementación de billetes

---
+++ testing_p2/src/main/java/testing_p2_3c1/billete.java
@@ -0,0 +1,72 @@
+public class billete {
+
+    private tipoBillete tipo;
+    private float precio;
+    private String ciudad_origen;
+    private String pais_origen;
+    private String ciudad_destino;
+    private String pais_destino;
+
+    public billete(String tipo, float precio, String c_origen, String p_origen, String c_destino, String p_destino){
+        this.tipo = tipo;
+        this.precio = precio;
+        this.ciudad_origen = c_origen;
+        this.ciudad_destino = c_destino;
+        this.pais_origen = p_origen;
+        this.pais_destino = p_destino;
+    }
+
+    public tipoBillete getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(tipoBillete tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getCiudad_origen() {
+        return ciudad_origen;
+    }
+
+    public void setCiudad_origen(String ciudad_origen) {
+        this.ciudad_origen = ciudad_origen;
+    }
+
+    public String getPais_origen() {
+        return pais_origen;
+    }
+
+    public void setPais_origen(String pais_origen) {
+        this.pais_origen = pais_origen;
+    }
+
+    public String getCiudad_destino() {
+        return ciudad_destino;
+    }
+
+    public void setCiudad_destino(String ciudad_destino) {
+        this.ciudad_destino = ciudad_destino;
+    }
+
+    public String getPais_destino() {
+        return pais_destino;
+    }
+
+    public void setPais_destino(String pais_destino) {
+        this.pais_destino = pais_destino;
+    }
+
+    public String toString() {
+        return "Billete [tipo=" + tipo + ", precio=" + precio + ", ciudad_origen=" + ciudad_origen + ", pais_origen="
+                + pais_origen + ", ciudad_destino=" + ciudad_destino + ", pais_destino=" + pais_destino + "]";
+    };
+    
+}

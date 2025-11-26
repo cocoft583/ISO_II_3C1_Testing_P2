4e276eb5402e2ab27aaa9c4b4c8fceb5def9e34d
Alberto <alberto.barato@alu.uclm.es>
2025-11-26 18:31:23 +0100

    Versión inicial de la clase Cliente finalizada

---
+++ testing_p2/src/main/java/testing_p2_3c1/Cliente.java
@@ -0,0 +1,142 @@
+package ISO_II_3C1_Testing_P2.testing_p2.src.main.java.testing_p2_3c1;
+
+public class Cliente {
+    private int edad;
+    private int frecuenciaViajes;
+    private char preferenciaClase; // Será clase turista o clase business;
+    private String destinoPreferido;
+    private int disponibilidadFinanciera; // Lo consideramos como un atributo en el que se define lo máximo que puede
+                                          // gastar el cliente.
+    private boolean ninos; // Los niños son aquellos viajeros menores de 12 años.
+    private boolean trabaja; // True --> Sí. False --> No.
+    private boolean independizado; // True --> Sí. False --> No.
+    private int ingresos;
+    private String domicilio; // El formato será algo como Calle, número, Ciudad , País. --> Separado por
+                              // comas. Y así podremos aplicar lo de split usando las "," (comas) como
+                              // separadores.
+    private String universidad; // El formato será como el de domicilio, indicando el nombre de la universidad,
+                                // la ciudad y el país.
+
+    public Cliente(int edad, int frecuenciaViajes, char preferenciaClase, String destinoPreferido,
+            int disponibilidadFinanciera, boolean ninos, boolean trabaja, boolean independizado, int ingresos,
+            String domicilio, String universidad) {
+        this.edad = edad;
+        this.frecuenciaViajes = frecuenciaViajes;
+        this.preferenciaClase = preferenciaClase;
+        this.destinoPreferido = destinoPreferido;
+        this.disponibilidadFinanciera = disponibilidadFinanciera;
+        this.ninos = ninos;
+        this.trabaja = trabaja;
+        this.independizado = independizado;
+        this.ingresos = ingresos;
+        this.domicilio = domicilio;
+        this.universidad = universidad;
+    }
+
+
+    //Métodos getter, setter y sobreescritura del toString().
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public int getFrecuenciaViajes(){
+        return frecuenciaViajes;
+    }
+
+    public char getPreferenciaClase(){
+        return preferenciaClase;
+    }
+
+    public String getDestinoPreferido(){
+        return destinoPreferido;
+    }
+
+    public int getDisponibilidadFinanciera(){
+        return disponibilidadFinanciera;
+    }
+
+    public boolean getNinos(){
+        return ninos;
+    }
+
+    public boolean getTrabaja(){
+        return trabaja;
+    }
+
+    public boolean getIndependizado(){
+        return independizado;
+    }
+
+    public int getIngresos(){
+        return ingresos;
+    }
+
+    public String getDomicilio(){
+        return domicilio;
+    }
+
+    public String getUniversidad(){
+        return universidad;
+    }
+
+    public void setEdad(int nuevaEdad){
+        edad = nuevaEdad;
+    }
+
+    public void setFrecuenciaViajes(int nuevaFrecuenciaViajes){
+        frecuenciaViajes = nuevaFrecuenciaViajes;
+    }
+
+    public void setPreferenciaClase(char nuevaPreferenciaClase){
+        preferenciaClase = nuevaPreferenciaClase;
+    }
+
+    public void setDestinoPreferido(String nuevoDestinoPreferido){
+        destinoPreferido = nuevoDestinoPreferido;
+    }
+
+    public void setDisponibilidadFinanciera(int nuevaDisponibilidadFinanciera){
+        disponibilidadFinanciera = nuevaDisponibilidadFinanciera;
+    }
+
+    public void setNinos(boolean actualizarNinos){
+        ninos = actualizarNinos;
+    }
+
+    public void setTrabaja(boolean actualizarTrabaja){
+        trabaja = actualizarTrabaja;
+    }
+
+    public void setIndependizado(boolean actualizarIndependizado){
+        independizado = actualizarIndependizado;
+    }
+
+    public void setIngresos(int nuevoIngreso){
+        ingresos = nuevoIngreso;
+    }
+
+    public void setDomicilio(String nuevoDomicilio){ç
+        domicilio = nuevoDomicilio;
+    }
+
+    public void setUniversidad(String nuevaUniversidad){
+        universidad = nuevaUniversidad;
+    }
+
+    @Override
+    public String toString(){
+        return "Cliente [edad=" + edad
+            + " ,frecuenciaViajes= " + frecuenciaViajes
+            + " ,preferenciaClase= " + preferenciaClase
+            + " ,destinoPreferido= " + destinoPreferido
+            + " ,disponibilidadFinanciera= " + disponibilidadFinanciera
+            + " ,ninos= " + ninos
+            + " ,trabaja= " + trabaja
+            + " ,independizado= " + independizado
+            + " ,ingresos= " + ingresos
+            + " ,domicilio= " + domicilio
+            + " ,universidad= " + universidad
+            + "]";
+    }
+}

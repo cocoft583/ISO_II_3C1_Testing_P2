ec914d0d00eb3b6c33f9d781d92c99a262ce2158
coco <jorge.assis@alu.uclm.es>
2025-12-02 13:43:36 +0100

    Borrado de la clase billete y implementación de la clase viajes

--- testing_p2/src/main/java/testing_p2_3c1/viaje.java
+++ testing_p2/src/main/java/testing_p2_3c1/viaje.java
@@ -1,3 +1,6 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class viaje {
@@ -8,6 +11,9 @@ public class viaje {
     private String origen;
     private boolean conNinos;
     private String motivo;
+    private LocalDate fecha;
+
+    private static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public viaje(String tipo, String destino, String origen, int billetes, boolean conNinos,
             String motivo) {
@@ -15,7 +21,7 @@ public class viaje {
         this.destino = destino;
         this.origen = origen;
         this.conNinos = conNinos;
-        this.billetes = billetes; //REVISAR ESTO: Se crea dentro de viajes la lista de billetes
+        this.billetes = crear_lista(billetes); //REVISAR ESTO: Se crea dentro de viajes la lista de billetes
 
         switch (tipo.toLowerCase()) {
             case "turista":
@@ -44,6 +50,22 @@ public class viaje {
 
             default:
                 throw new IllegalArgumentException("Motivo de viaje desconocido");
+        }
+    }
+
+    private List<billete> crear_lista(int billetes) {
+        for (int i = 0; i < billetes; i++) {
+            this.billetes.add(new billete());
+        }   
+        return null;
+    }
+
+    public LocalDate parse_fecha(String fecha){
+        try {
+            return LocalDate.parse(fecha, FORMATO_FECHA);
+        } catch (DateTimeParseException e) {
+                System.out.println("Fecha inválida: " + textoFecha);
+                return null; // o lanzar tu propia excepción, según te convenga
         }
     }
 }

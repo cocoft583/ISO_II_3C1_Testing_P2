a6ce446d7081fe9ec96ebacec266ec14a2f216bf
adriandelgaado9 <adrian.delgado1@alu.uclm.es>
2025-12-18 12:37:01 +0100

    "modificacion del pom para utilizar junit con surefile y jacoco"

---
+++ testing_p2/src/test/java/testing_p2_3c1/BilleteTest.java
@@ -0,0 +1,11 @@
+package testing_p2_3c1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BilleteTest {
+
+    @Test
+    void testHelloWorld() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
